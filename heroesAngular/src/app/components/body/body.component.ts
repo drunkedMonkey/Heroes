3f839840468a9d18080313917d228b277f9b459f
José Jesús Ibañez Cantos <josejesusibanezcantos@MacBook-Pro-de-Jose.local>
2019-12-19 21:51:20 +0100

    creo el body

---
+++ heroesAngular/src/app/components/body/body.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { template } from '@angular/core/src/render3';
+
+@Component({
+    selector: 'app-body',
+    templateUrl: './body.component.html'
+})
+export class BodyComponent {
+    
+}
+
