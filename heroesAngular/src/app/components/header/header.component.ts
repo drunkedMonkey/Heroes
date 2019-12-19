20b72b0498d3ea2cb105aaeb3573d95f7681253a
José Jesús Ibañez Cantos <josejesusibanezcantos@MacBook-Pro-de-Jose.local>
2019-12-19 21:32:19 +0100

    creacion del primer componente, header

---
+++ heroesAngular/src/app/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'app-header',
+   template: `<h1>Hola mundo, este es el primer componente</h1>` 
+})
+export class HeaderComponent{
+
+}
