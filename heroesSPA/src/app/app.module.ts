7258df5273890202b4b61f7317062dade3dd5421
José Jesús Ibañez Cantos <josejesusibanezcantos@MacBook-Pro-de-Jose.local>
2019-12-26 18:58:49 +0100

    Creo el primer servicio, el que va a manejar los heroes

--- heroesSPA/src/app/app.module.ts
+++ heroesSPA/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 //Rutas
  import { APP_ROUTING } from "./app.routes";
 
+ //Servicios
+ import { HeroesService } from "./services/heroes.service";
+
 //Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
@@ -23,7 +26,9 @@ import { HeroesComponent } from './components/shared/heroes/heroes.component';
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- heroesSPA/src/app/components/shared/heroes/heroes.component.ts
+++ heroesSPA/src/app/components/shared/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HeroesService, Heroe } from "../../../services/heroes.service";
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes:Heroe[] = [];
+
+  constructor( private _heroesService:HeroesService) { 
+
+  }
 
   ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
   }
 
 }
