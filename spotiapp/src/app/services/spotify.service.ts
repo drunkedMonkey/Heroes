cdd001cff19c6e810ebb8d43f335cf1a175cc691
José Jesús Ibañez Cantos <josejesusibanezcantos@MacBook-Pro-de-Jose.local>
2020-01-20 18:50:28 +0100

    centralizacion de peticiones a spotify

--- spotiapp/src/app/services/spotify.service.ts
+++ spotiapp/src/app/services/spotify.service.ts
@@ -8,23 +8,22 @@ export class SpotifyService {
 
   constructor(private http: HttpClient) { }
 
-  getNewReleases(){
+  getQuery(query:string){
+    const url = `https://api.spotify.com/v1/${query}`;
     const headers = new HttpHeaders({
       'Authorization': 'Bearer BQA9x0r5sKZan0M_TOzPoffpUJMyCEuodv_VaWOsrZ3jkY5Y8gBbr3kIM1_lpVxKtDNM0tTJbpxEFJRKUe8'
-    
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases',{ headers})
-    .pipe(map(data => data['albums'].items));
+    return this.http.get(url, {headers});
+  }
+
+  getNewReleases(){    
+    return this.getQuery('browse/new-releases?limit=20')
+              .pipe(map(data => data['albums'].items));    
   }
 
   getArtista(termino:string){
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCfGWsPkhVRV_UMAVADgrwgFpyqmjVgBc9FhnxGBAgNiMhPxpk1a43bh0tL6CD2_yItkpeLOJI0VQF24yw'
-    
-    });
-
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`,{ headers })
-    .pipe(map (data => data['artists'].items));
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+              .pipe(map (data => data['artists'].items));
   }
 }
